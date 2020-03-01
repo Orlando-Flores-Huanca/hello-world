d57699a393d871ccd232ca4987144e4bcae28644
Orlando-Flores-Huanca <54519294+Orlando-Flores-Huanca@users.noreply.github.com>
2020-03-01 00:47:11 -0400

    Create autonuevo.js

---
+++ autonuevo.js
@@ -0,0 +1,20 @@
+//función constructora
+function auto(marca, modelo, anio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+  }
+
+//declarar un array vacio
+  var autos = [];
+
+// crear nuevos objetos con la función constructora
+  for(i= 0; i<30; i++){
+    var aut =new auto("Toyota"+i, "Modelo" + i, 1990 + i);
+    autos.push(aut);
+  }
+  
+//mostrar el array 
+  for(i= 0; i<30; i++){
+   console.log(`Marca: ${autos[i].marca} Modelo : ${autos[i].modelo} Año ${autos[i].anio}`) 
+  }
